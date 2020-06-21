eb1ce3069b5968be3a6ce284662bda8f29ba4b37
Vinay <vardhivinaykumar@gmail.com>
2020-06-21 19:33:30 +0530

    Initial

---
+++ Java8_StreamsAndLambdas/src/main/java/com/vinay/Java8_StreamsAndLambdas/App.java
@@ -0,0 +1,13 @@
+package com.vinay.Java8_StreamsAndLambdas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
